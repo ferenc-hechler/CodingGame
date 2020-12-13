37490102348d0281dd30ec76a46a14ca235c5b1d
feri <github@fh.anderemails.de>
2020-12-13 03:33:40 +0100

    day12 part2 aoc.

--- src/de/hechler/adventofcode/Day12.java
+++ src/de/hechler/adventofcode/Day12.java
@@ -90,10 +90,85 @@ public class Day12 {
 	}
 
 	public static void mainPart2() throws FileNotFoundException {
+		try (Scanner scanner = new Scanner(new File("input/day12.txt"))) {
+			while (scanner.hasNext()) {
+				String word = scanner.next();
+				char cmd = word.charAt(0);
+				int num = Integer.parseInt(word.substring(1));
+				switch (cmd) {
+				case 'E': {
+					moveWP(0, num);
+					break;
+				}
+				case 'S': {
+					moveWP(1, num);
+					break;
+				}
+				case 'W': {
+					moveWP(2, num);
+					break;
+				}
+				case 'N': {
+					moveWP(3, num);
+					break;
+				}
+				case 'F': {
+					forward(num);
+					break;
+				}
+				case 'L': {
+					rotWPL(num);
+					break;
+				}
+				case 'R': {
+					rotWPR(num);
+					break;
+				}
+				}
+			}
+		}
+		System.out.println(Math.abs(posX)+Math.abs(posY));
 	}
 
+	private static void forward(int num) {
+		posX += num* wpX;
+		posY += num* wpY;
+	}
+
+	private static void moveWP(int dir, int num) {
+		while (dir < 0) {
+			dir = dir + 4;
+		}
+		while (dir >= 4) {
+			dir = dir - 4;
+		}
+		wpX += dx[dir]*num;
+		wpY += dy[dir]*num;
+	}
+
+	private static void rotWPL(int num) {
+		rotWPR(360-num);
+	}
+
+	private static void rotWPR(int num) {
+		while (num < 0) {
+			num += 360;
+		}
+		while (num >= 360) {
+			num -= 360;
+		}
+		int r = num / 90;
+		for (int i=0; i<r; i++) {
+			int oldWPX = wpX;
+			int oldWPY = wpY;
+			wpX = oldWPY;
+			wpY = -oldWPX;
+		}
+	}
+
+
 	public static void main(String[] args) throws FileNotFoundException {
-		mainPart1();
+		mainPart2();
 	}
 
 	
