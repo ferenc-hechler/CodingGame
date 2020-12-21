e46a2ce354937f97b037ecd2828edc55c84aa518
ferenc.hechler@t-systems.com <ferenc.hechler@t-systems.com>
2020-12-21 09:21:44 +0100

    day21 part2 aoc.

--- src/de/hechler/adventofcode/Day21.java
+++ src/de/hechler/adventofcode/Day21.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -86,6 +87,84 @@ public class Day21 {
 		}
 	}
 
+	public static void mainPart2() throws FileNotFoundException {
+		try (Scanner scanner = new Scanner(new File("input/day21.txt"))) {
+			foods = new ArrayList<>();
+			allergene2foodMap = new LinkedHashMap<>();
+			ingredients2foodMap = new LinkedHashMap<>();
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine().trim();
+				if (line.isEmpty()) {
+					continue;
+				}
+				if (!line.matches(CONTAINS_RX)) {
+					throw new RuntimeException("invvalid contains line: '"+line+"'");
+				}
+				String[] ingredients = line.replaceFirst(CONTAINS_RX, "$1").trim().split(" "); 
+				String[] allergenes = line.replaceFirst(CONTAINS_RX, "$2").trim().split(", ");
+				Food food = new Food(ingredients, allergenes);
+				add2maps(food);
+			}
+			System.out.println(foods);
+			allergene2ingredientsMap = new LinkedHashMap<>();
+			ingredientsWithAllergenes = new LinkedHashSet<>();
+			for (String allergene:allergene2foodMap.keySet()) {
+				Set<Food> foodList = allergene2foodMap.get(allergene);
+				Set<String> commonIngredients = null;
+				for (Food food:foodList) {
+					if (commonIngredients == null) {
+						commonIngredients = new LinkedHashSet<>(food.ingredients);
+					}
+					else {
+						commonIngredients.retainAll(food.ingredients);
+					}
+				}
+				System.out.println(allergene+": "+commonIngredients);
+				allergene2ingredientsMap.put(allergene, commonIngredients);
+				ingredientsWithAllergenes.addAll(commonIngredients);
+			}
+
+			Map<String, Set<String>> reducedAllergene2ingredientsMap = new LinkedHashMap<>();
+			for (String allergene:allergene2ingredientsMap.keySet()) {
+				reducedAllergene2ingredientsMap.put(allergene, new LinkedHashSet<>(allergene2ingredientsMap.get(allergene)));
+			}
+			
+			boolean changes = true;
+			while (changes) {
+				changes = false; 
+				for (String allergene:reducedAllergene2ingredientsMap.keySet()) {
+					if (reducedAllergene2ingredientsMap.get(allergene).size() == 1) {
+						String ing = reducedAllergene2ingredientsMap.get(allergene).iterator().next();
+						for (String a:reducedAllergene2ingredientsMap.keySet()) {
+							Set<String> ings = reducedAllergene2ingredientsMap.get(a);
+							if (ings.size() > 1) {
+								if (ings.remove(ing)) {
+									changes = true;
+								}
+							}
+						}
+						if (changes) {
+							break;
+						}
+					}
+				}
+			}
+
+			for (String allergene:reducedAllergene2ingredientsMap.keySet()) {
+				System.out.println("R-"+allergene+": "+reducedAllergene2ingredientsMap.get(allergene));
+			}
+			
+			List<String> sortAlgs = new ArrayList<>();
+			reducedAllergene2ingredientsMap.keySet().forEach(alg -> sortAlgs.add(alg));
+			Collections.sort(sortAlgs);
+			String seperator = "";
+			for (String alg:sortAlgs) {
+				System.out.print(seperator+reducedAllergene2ingredientsMap.get(alg).iterator().next());
+				seperator = ",";
+			}
+		}
+	}
+
 	private static void add2maps(Food food) {
 		foods.add(food);
 		for (String ingredient:food.ingredients) {
@@ -107,7 +186,7 @@ public class Day21 {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		mainPart1();
+		mainPart2();
 	}
 
 	
