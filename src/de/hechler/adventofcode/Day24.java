f51062ae1d6b716402256e5785838fb9d2772ef4
feri <github@fh.anderemails.de>
2020-12-24 20:31:59 +0100

    aoc day24 part2.

--- src/de/hechler/adventofcode/Day24.java
+++ src/de/hechler/adventofcode/Day24.java
@@ -25,6 +25,9 @@ public class Day24 {
 			this.row = row;
 			this.col = col;
 		}
+		public HexPos(HexPos hex) {
+			this(hex.row, hex.col);
+		}
 		public void go(String dir) {
 			switch (dir) {
 			case "e":
@@ -49,6 +52,14 @@ public class Day24 {
 				break;
 			}
 		}
+		public void min(HexPos hex) {
+			row = Math.min(row, hex.row);
+			col = Math.min(col, hex.col);
+		}
+		public void max(HexPos hex) {
+			row = Math.max(row, hex.row);
+			col = Math.max(col, hex.col);
+		}
 		@Override
 		public int hashCode() {
 			final int prime = 31;
@@ -72,10 +83,15 @@ public class Day24 {
 				return false;
 			return true;
 		}
+		@Override
+			public String toString() {
+				return "H<"+row+","+col+">";
+			}
 	}
+
+	static Set<HexPos> turnedHexPosSet = new LinkedHashSet<>();
 	
 	public static void mainPart1() throws FileNotFoundException {
-		Set<HexPos> turnedHexPosSet = new LinkedHashSet<>();
 		try (Scanner scanner = new Scanner(new File("input/day24.txt"))) {
 			while (scanner.hasNext()) {
 				String way = scanner.next();
@@ -91,8 +107,6 @@ public class Day24 {
 			System.out.println("#black: "+turnedHexPosSet.size());
 		}
 	}
-	
-	
 	
 	private static HexPos goHexPos(String way) {
 		HexPos result = new HexPos();
@@ -112,9 +126,83 @@ public class Day24 {
 	}
 
 
+	static HexPos minHex;
+	static HexPos maxHex;
+	
+	public static void mainPart2() throws FileNotFoundException {
+		try (Scanner scanner = new Scanner(new File("input/day24.txt"))) {
+			while (scanner.hasNext()) {
+				String way = scanner.next();
+				HexPos hexPos = goHexPos(way);
+				if (turnedHexPosSet.contains(hexPos)) {
+					turnedHexPosSet.remove(hexPos);
+				}
+				else {
+					turnedHexPosSet.add(hexPos);
+				}
+			}
+			System.out.println(turnedHexPosSet);
+			System.out.println("#black: "+turnedHexPosSet.size());
+		}
+		for (int i=0; i<=100; i++) {
+			System.out.println(i+": "+turnedHexPosSet.size());
+			Set<HexPos> nextTurnedHexPosSet = new LinkedHashSet<>();
+			getMinMax();
+			for (int row=minHex.row-1; row<=maxHex.row+1; row++) {
+				for (int col=minHex.col-1; col<=maxHex.col+1; col++) {
+					HexPos check = new HexPos(row, col);
+					int count = countNeigbours(check);
+					if (turnedHexPosSet.contains(check)) {
+						// black: keep if 1 or 2 neighbours
+						if ((count == 1) || (count == 2)) {
+							nextTurnedHexPosSet.add(check);
+						}
+					}
+					else {
+						// white: flip if 2 neighbours
+						if (count == 2) {
+							nextTurnedHexPosSet.add(check);
+						}
+					}
+				}
+			}
+			turnedHexPosSet = nextTurnedHexPosSet;
+		}
+	}
+	
+	
+
+	private static int countNeigbours(HexPos check) {
+		int result = 0;
+		check.go("e");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("sw");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("w");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("nw");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("ne");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("e");
+		result += turnedHexPosSet.contains(check) ? 1 : 0; 
+		check.go("sw");
+		return result;
+	}
+
+	private static void getMinMax() {
+		minHex = new HexPos(turnedHexPosSet.iterator().next());
+		maxHex = new HexPos(minHex);
+		for (HexPos hex:turnedHexPosSet) {
+			minHex.min(hex);
+			maxHex.max(hex);
+		}
+	}
+	
+	
 
 	public static void main(String[] args) throws FileNotFoundException {
-		mainPart1();
+		mainPart2();
 	}
 
 	
